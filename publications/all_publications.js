02471e966206bfa78a0f524aa4f46f2d559f7742
kocatekin <116370462+kocatekin@users.noreply.github.com>
2024-12-31 15:23:19 +0300

    Create all_publications.js

---
+++ publications/all_publications.js
@@ -0,0 +1,45 @@
+let data = 
+{
+   "SCI_Journals": [
+       /*{
+           "title": "Exploring the Impact of Functional Programming on Modern Software Development",
+           "journal": "Journal of Software Engineering",
+           "publicationDate": "May 2025",
+           "authors": ["Dr. John Doe", "Dr. Jane Smith"],
+           "abstract": "This paper investigates how functional programming paradigms have influenced the development of contemporary software applications, focusing on immutability, first-class functions, and state management.",
+           "doi": "10.1000/jse.2025.123456",
+           "keywords": ["Functional Programming", "Software Development", "Immutability"]
+       }*/
+   ],
+   "Non_SCI_Journals": [
+       {
+           "title": "Evolution and State of the Art in Password Storage",
+           "journal": "Eskişehir Türk Dünyası Uygulama ve Araştırma Merkezi Bilişim Dergisi",
+           "publicationDate": "2023",
+           "authors": ["Dr. Tuğberk Kocatekin"],
+           "abstract": "Passwords have historically been pivotal for access control and authentication, yet their security remains a recurring concern in today’s digital world. As evidenced by high-profile data breaches, secure password storage has always been paramount, but often not achieved. While users grapple with the creation of strong, memorable passwords, the burden also falls on service providers to store these passwords securely. Even though alternative authentication mechanisms have emerged, password-based authentication remains pervasive. Surprisingly, studies highlight that developers frequently exhibit misconceptions or negligence towards password storage security. This paper traces the progression of password storage methods by explaining four password hashing methods. By informing of four modern password storage systems, this work seeks to bridge the knowledge gap, advocating for better practices and illuminating the significance of prioritizing security alongside functionality.",
+           "doi": "10.53608/estudambilisim.1318760",
+           "link":"https://dergipark.org.tr/en/download/article-file/3227979",
+       }
+   ],
+   "Conferences": [
+       {
+           "title": "Adaptation of n-out-of-n secret sharing scheme into IoT network",
+           "conference": "2023 16th International Conference on Information Security and Cryptology (ISCTürkiye)",
+           "publicationDate": "2023",
+           "abstract":"Internet of Things (IoT) has become an established part of our daily lives by interconnecting billions of devices in diverse areas such as healthcare, smart home technologies, agriculture, etc. However, these devices are limited in memory, energy and computational capabilities. This creates a great potential for security issues to arise as being constrained prevents them from applying complex cryptographic algorithms. In this study, we propose a novel method to provide a low-cost and secure communication for constrained IoT devices. The proposed method is based on a n-out-of-n secret sharing scheme and mimicks the idea of visual cryptography in a digital set-up.Generally, when an IoT device communicates with an outer party, it establishes the communication by itself or through a mediary such as a central hub or gateway; which leads to single point of failure. Our proposed method aims for a distributed environment in which devices collaborate with each other and therefore divide the responsibility of sending a message into multiple devices, instead of just one device. Therefore, when a device plans on sending a message, all its neighbors send it on the behalf of this device.",
+           "authors": ["Dr. Tuğberk Kocatekin", "Dr. Cafer Çalışkan"],
+           "doi": "10.1109/ISCTrkiye61151.2023.10336050",
+           "link": "https://ieeexplore.ieee.org/abstract/document/10336050/"
+       },
+       {
+         "title": "Text Mining in Radiology Reports",
+         "conference": "2013 21st Signal Processing and Communications Applications Conference (SIU)",
+         "publicationDate": "2013",
+         "abstract":"Text mining is a popular research topic with application areas ranging from security to media and marketing. More specifically, text mining has been applied in biomedical area for the categorization of radiology reports, which is a challenging problem due to their free-text and unstructured format. State-of-the-art in radiology report mining has mostly focused on English text, while studies on Turkish reports are scarce. Accordingly, in this work we propose to employ text mining for categorization of Turkish radiology reports. We automatically remove header and footer of the reports, apply frequency analysis on the remaining report text, and perform categorization of reports to anatomical regions using pre-selected keywords. The accuracy of the proposed solution is measured as 84.3% over a 66-report test set.",
+         "authors": ["Tuğberk Kocatekin", "Dr. Devrim Ünay"],
+         "doi": "10.1109/SIU.2013.6531400",
+         "link": "https://ieeexplore.ieee.org/abstract/document/6531400"
+     }
+   ]
+}
